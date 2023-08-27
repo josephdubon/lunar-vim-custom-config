1b53e4d660263cba56aed04284dcbf780097a938
Joseph Dubon <josephdubon@pm.me>
2023-08-27 12:19:57 -0700

    added lint formatter
    for use with js ts react

--- lua/user/lint-format-config.lua
+++ lua/user/lint-format-config.lua
@@ -9,17 +9,23 @@
 --
 -- Linters Config
 local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-    { command = "eslint", filetypes = { "typescript", "typescriptreact" } }
-}
+linters.setup {{ 
+    command = "eslint", 
+    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" } 
+  }}
 
 -- Formatter Config
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-    {
-        command = "prettier",
-        filetypes = { "typescript", "typescriptreact" },
-    },
+{
+    name = "prettier",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespace
+    -- options such as `--line-width 80` become either `{"--line-width", "80"}` or `{"--line-width=80"}`
+    args = {"--semi", "false"}, {"--single-quote", "false"},
+    ---@usage only start in these filetypes, by default it will attach to all filetypes it supports
+    filetypes = { "typescript", "typescriptreact" },
+  },
 }
 
 -- TailwindCSS Config
